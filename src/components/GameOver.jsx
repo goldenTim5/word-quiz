790bb513f5c8fdc85da173061a62f10606c5e5b5
thomasL <thomas.el.8258@gmail.com>
2023-12-12 15:48:21 +0000

    working version

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 
 const GameOver = ({ score, setGameState }) => {
-  return (
-    <>
-      <div id="gameover-screen">
-        <h1>Game Over</h1>
-        <h2>You Scored {score} points </h2>
-        {/* <img src={`logo.svg`} alt={`${logo} Icon`} height="100px" width="100px" /> */}
-        <p>Click on the button below to start a new game</p>
-      </div>
-      <button className="restart-game" aria-label="restart-game">
-        onClick={() => setGameState(0)}
-        Play Again
-      </button>
-    </>
-  );
+	return (
+		<>
+			<div id="gameover-screen">
+				<h1>Game Over</h1>
+				<h2>You Scored {score / 2} points </h2>
+				{/* <img src={`logo.svg`} alt={`${logo} Icon`} height="100px" width="100px" /> */}
+				<p>Click on the button below to start a new game</p>
+			</div>
+			<button
+				className="restart-game"
+				aria-label="restart-game"
+				onClick={() => setGameState(1)}
+			>
+				Play Again
+			</button>
+		</>
+	);
 };
 
 export default GameOver;
