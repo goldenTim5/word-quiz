72d2f2a71f02a6a32f69ef4786bf014792dbc520
Raypenver <raypenver101@outlook.com>
2023-12-12 16:44:27 +0000

    styled start, game, andgame over screens

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,26 +1,24 @@
 import React from "react";
+import "../styles/start-end.scss";
 
 const Start = ({ setGameState }) => {
   return (
     <>
-      <div id="start-screen">
+      <div className="start-end-screen">
         <h1>Start Game</h1>
         <h1>Welcome to What3Letters</h1>
-        <img
-          src={`logo.svg`}
-          alt={`${logo} Icon`}
-          height="100px"
-          width="100px"
-        />
-        <p>Click on the button below to start a new game</p>
+        <img src={`whatthreeletters.svg`} height="100px" width="100px" />
+        <p className="start-instructions">
+          Click on the button below to start a new game
+        </p>
+        <button
+          className="submit-button"
+          aria-label="start-game"
+          onClick={() => setGameState(1)}
+        >
+          Start Game
+        </button>
       </div>
-      <button
-        className="start-game"
-        aria-label="start-game"
-        onClick={() => setGameState(1)}
-      >
-        Start Game
-      </button>
     </>
   );
 };
